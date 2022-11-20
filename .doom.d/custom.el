9579b0f22a9d3640f6481ccdc4edd8d62f947637
edapess <edmondavetisyanw@gmail.com>
2022-11-20 19:21:12 +0300

    feat: doom emacs wakatime

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -6,11 +6,11 @@
  '(custom-safe-themes
    '("c2aeb1bd4aa80f1e4f95746bda040aafb78b1808de07d340007ba898efa484f5" default))
  '(global-wakatime-mode t)
- '(org-agenda-files '("/home/zick/Documents/org/simpleTodo.org"))
+ '(org-agenda-files '("/home/edapess/Documents/org/simpleTodo.org"))
  '(package-selected-packages
    '(lsp-dart dart-mode one-themes rjsx-mode tide wakatime-mode))
  '(wakatime-api-key "94239320-6d7c-4864-a5cf-62453eaa2b20")
- '(wakatime-cli-path "/home/zick/.wakatime/wakatime-cli-linux-amd64"))
+ '(wakatime-cli-path "/home/edapess/.wakatime/wakatime-cli-linux-amd64"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
