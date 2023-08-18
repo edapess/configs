aede3a899e5654eb269a0995522276e78cab4b81
edapess <edmondavetisyanw@gmail.com>
2023-08-19 00:18:43 +0400

    init layouts

---
+++ qtile/layouts.py
@@ -0,0 +1,18 @@
+from libqtile import layout
+
+def init_layouts():
+    return [
+    layout.Max(),
+    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    # Try more layouts by unleashing below layouts.
+    layout.Stack(num_stacks=2),
+    layout.Bsp(),
+    layout.Matrix(),
+    layout.MonadTall(),
+    layout.MonadWide(),
+    layout.RatioTile(),
+    layout.Tile(),
+    layout.TreeTab(),
+    layout.VerticalTile(),
+    layout.Zoomy(),
+]
